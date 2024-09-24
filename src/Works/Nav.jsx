5ebb3e9a6e00f97259a9294eefc9c1f0b014d488
Prassadraj <prassadpr10@gmail.com>
2024-09-24 12:42:50 +0530

    chg

--- src/Works/Nav.jsx
+++ src/Works/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Nav() {
   return (
-    <div className="w-full h-[3rem] laptop:h-[6rem] tablet:h-[4rem] bg-white fixed border-b-2 top-0 left-0 z-10">
+    <div className="w-full h-[3rem] laptop:h-[4rem] tablet:h-[4rem] bg-white fixed border-b-2 top-0 left-0 z-10">
       <div className="text-xl tablet:text-2xl laptop:text-4xl font-semibold flex h-full w-full justify-center items-center">
         <p>Estilo Fashions</p>
       </div>
