8542a8f219b7cf0f74739bba3566a5efd60b9e7d
Prassadraj <prassadpr10@gmail.com>
2024-10-03 17:23:19 +0530

    chg

--- src/Works/Nav.jsx
+++ src/Works/Nav.jsx
@@ -6,7 +6,12 @@ function Nav() {
     <div className="w-full flex justify-around h-[3rem] laptop:h-[4rem] tablet:h-[4rem] bg-white fixed border-b-2 top-0 left-0 z-10">
       <div className="text-xl tablet:text-2xl laptop:text-4xl font-semibold flex h-full w-full justify-center items-center">
         <div>
-          <Image src="/estilo.png" width={150} height={200} />
+          <Image
+            src="/estilo.png"
+            className="w-[100px] tablet:w-[150px] object-cover"
+            width={50}
+            height={200}
+          />
         </div>
       </div>
     </div>
