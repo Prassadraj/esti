96587e7539c245f644561fee44e5c9c20586454f
Prassadraj <prassadpr10@gmail.com>
2024-10-03 17:00:33 +0530

    chg

--- src/Works/Nav.jsx
+++ src/Works/Nav.jsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 function Nav() {
   return (
-    <div className="w-full h-[3rem] laptop:h-[4rem] tablet:h-[4rem] bg-white fixed border-b-2 top-0 left-0 z-10">
+    <div className="w-full flex justify-around h-[3rem] laptop:h-[4rem] tablet:h-[4rem] bg-white fixed border-b-2 top-0 left-0 z-10">
       <div className="text-xl tablet:text-2xl laptop:text-4xl font-semibold flex h-full w-full justify-center items-center">
-        <p>Estilo Fashions</p>
+        <div>
+          <Image src="/estilo.png" width={150} height={200} />
+        </div>
       </div>
     </div>
   );
